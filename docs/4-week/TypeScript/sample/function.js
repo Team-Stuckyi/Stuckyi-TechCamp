62854728765b97676c446245c16de10656835dd6
Xeros <xeros.log@gmail.com>
2022-02-07 13:17:18 +0900

    Update function.js

--- docs/4-week/TypeScript/sample/function.js
+++ docs/4-week/TypeScript/sample/function.js
@@ -8,6 +8,7 @@ function log(a, b, c) {
     console.log(a);
 }
 log("Hello", "World", '!');
-log(String(sum(1, 5)));
 //  에러 발생 (log 함수에서 a 인자는 문자형이어야하는데 number가 대입됨)
 // log(sum(1, 5))
+// 정상작동
+log(String(sum(1, 5)));
